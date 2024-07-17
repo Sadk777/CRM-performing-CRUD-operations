114c37f076cdd94b69584e3dff53c8ba8727d62c
Sadk777 <saddamsher756@gmail.com>
2024-07-17 18:16:14 +0500

    working on routes

--- routes/postRoute.js
+++ routes/postRoute.js
@@ -7,3 +7,24 @@ postroute.use(bodyParser.urlencoded({extended:true}));
 
 const multer = require('multer')
 const path = require('path');
+
+postroute.use(express.static('public'));
+multer.diskStorage({
+    destination: function(req, file, cb){
+        cb(null, path.join(__dirname, '../public/postImages'),function(error,success){
+            if (error){
+                console.log(error);
+            }
+        });
+    },
+    filename: function(req, file, cb){
+        const name = Date.now()+'-'+file.originalname;
+        cb(null,name,function(error,success){
+            if (error){
+                console.log(error);
+            }
+
+        })
+
+    }
+})
